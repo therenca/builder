2eb1cfd881844ea6111bbe41294e60ddadc6520e
Therenca Mureithi <therencamureithi@gmail.com>
2021-02-09 05:21:53 +0300

    builder for dart projects, stable

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,89 @@
 
-void main(List<String> arguments) {
-	
+import 'dart:io';
+import 'package:leo/leo.dart'as leo;
+import 'package:yaml/yaml.dart' as yaml;
+import 'package:builder/builder/build.dart';
+
+void main(List<String> args) async {
+
+	if(args.isNotEmpty && args.length > 1){
+
+		var _getFileHelper = () async {
+			var yamlFile = File(args[1]);
+			return await yamlFile.readAsString();
+		};
+
+		var future = _getFileHelper();
+		var yamlBlob = await leo.tryCatch(future);
+
+		if(yamlBlob != null){
+			var yamlDoc = yaml.loadYaml(yamlBlob);
+
+			print('yaml doc: $yamlDoc');
+
+			var targetFolder = yamlDoc['folder'];
+			var confFiles = yamlDoc['conf'];
+			var toBuild = yamlDoc['build'];
+
+			var ip;
+			var user;
+			var remotePath;
+			var syncInfo = yamlDoc['rsync'];
+			if(syncInfo != null){
+				ip = syncInfo['ip'];
+				user = syncInfo['user'];
+				remotePath = syncInfo['path'];
+			}
+
+			targetFolder ??= 'production';
+			confFiles ??= <yaml.YamlList>[];
+			toBuild ??= <yaml.YamlList>[];
+
+			switch(args.first){
+
+				case 'config': {
+
+					Build.mkdir(targetFolder);
+					Build.copy(confFiles, targetFolder);
+					Build.rsync(ip, user, remotePath, targetFolder);
+					Build.rm(targetFolder);
+
+					break;
+				}
+
+				case 'build': {
+					var toBuild;
+
+					Build.mkdir(targetFolder);
+					Build.aot(toBuild, targetFolder);
+					Build.rsync(ip, user, remotePath, targetFolder);
+					Build.rm(targetFolder);
+
+					break;
+				}
+
+				case 'all': {
+
+					Build.mkdir(targetFolder);
+					Build.aot(toBuild, targetFolder);
+					Build.copy(confFiles, targetFolder);
+					Build.rsync(ip, user, remotePath, targetFolder);
+					Build.rm(targetFolder);
+
+					break;
+				}
+
+				default: {
+
+					leo.pretifyOutput('[BUILDER] expected values are action "config" or "build" or "all"', color: 'red');
+
+					break;
+				}
+			}
+		} else {
+			leo.pretifyOutput('[BUILDER] yaml file: ${args[1]} does not exist', color: 'red');
+		}
+	} else {
+		leo.pretifyOutput('[BUILDER] only one action needed, either action "config" or "build" or "all", also specify the path to the yaml file. EG: ./builder config /path/to/yaml', color: 'red');
+	}
 }
---
+++ lib/builder/build.dart
@@ -0,0 +1,61 @@
+import 'package:dcli/dcli.dart';
+import 'package:leo/leo.dart' as leo;
+import 'package:yaml/yaml.dart' as yaml;
+
+class Build {	
+	static void mkdir(String folder){
+		try {
+			'mkdir $folder'.run;
+			'mkdir $folder/conf/'.run;
+		} catch(e){
+			leo.pretifyOutput('[BUILDER | $folder] unable to run mkdir', color: 'red');
+		}
+	}
+
+	static void aot(yaml.YamlList toBuild, String targetFolder){
+		toBuild.forEach((build) {
+			var isMove = false;
+			var buildParts = build.split(',');
+			var toBuild = buildParts[0];
+			var builtTo = buildParts[1];
+			
+			try {
+				'dart2native $toBuild -o $builtTo'.run;
+				isMove = true;
+			} catch(e){
+				leo.pretifyOutput('[BUILDER | aot] unable to build $toBuild', color: 'red');
+			}
+
+			if(isMove){
+				try {
+					'mv -v $builtTo $targetFolder/'.run;
+				} catch(e){
+					leo.pretifyOutput('[BUILDER | move] unable to move $builtTo to $targetFolder', color: 'red');
+				}
+			}
+		});
+	}
+
+	static void copy(yaml.YamlList confFolders, String targetFolder){
+
+		confFolders.forEach((conf) {
+			try {
+				'cp -rvf $conf $targetFolder/'.run;
+			} catch(e){
+				leo.pretifyOutput('[BUILDER | copy | $conf] unable to run copy', color: 'red');
+			}
+		});
+	}
+
+	static rsync(String ip, String user, String path, String targetFolder){
+		try {
+			"rsync -rvz -e 'ssh' --progress $targetFolder $user@$ip:path".run;
+		} catch(e){
+			leo.pretifyOutput('[BUILDER | sync] unable to sync with $ip', color: 'red');
+		}
+	}
+
+	static rm(String path){
+		"rm -rvf $path".run;
+	}
+}
